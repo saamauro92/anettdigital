f49341d689e18b6ed17c1d6846febbd2eeb98ab7
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-23 13:01:19 +0100

    fix: paths

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home'
-import Footer from './components/pages/Footer/Footer'
+import Footer from './components/Footer/Footer'
 import Services from "./components/pages/Services/Services"
 import ScrollToTop from './components/ScrollToTop';
 
